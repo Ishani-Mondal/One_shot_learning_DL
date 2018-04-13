acad14a91f58b6aed557ff4e41a94d09b36ae021
paheli <paheli@users.noreply.github.com>
2018-04-14 00:40:57 +0530

    Add files via upload

---
+++ knn.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 13 15:51:40 2018
+
+@author: user
+"""
+
+# import the necessary packages
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import train_test_split
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+
+def image_to_feature_vector(image, size=(32, 32)):
+	# resize the image to a fixed size, then flatten the image into
+	# a list of raw pixel intensities
+	return cv2.resize(image, size).flatten()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-k", "--neighbors", type=int, default=1,
+	help="# of nearest neighbors for classification")
+ap.add_argument("-j", "--jobs", type=int, default=-1,
+	help="# of jobs for k-NN distance (-1 uses all available cores)")
+args = vars(ap.parse_args())
+
+
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+ 
+# initialize the raw pixel intensities matrix, the features matrix,
+# and labels list
+rawImages = []
+features = []
+labels = []
+
+for (i, imagePath) in enumerate(imagePaths):
+	# load the image and extract the class label (assuming that our
+	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
+	image = cv2.imread(imagePath)
+	label = imagePath.split(os.path.sep)[-1].split(".")[0]
+ 
+	# extract raw pixel intensity "features", followed by a color
+	# histogram to characterize the color distribution of the pixels
+	# in the image
+	pixels = image_to_feature_vector(image)
+	
+ 
+	# update the raw images, features, and labels matricies,
+	# respectively
+	rawImages.append(pixels)
+	
+	labels.append(label)
+ 
+	# show an update every 1,000 images
+	if i > 0 and i % 1000 == 0:
+		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
+
+rawImages = np.array(rawImages)
+features = np.array(features)
+labels = np.array(labels)
+print("[INFO] pixels matrix: {:.2f}MB".format(
+	rawImages.nbytes / (1024 * 1000.0)))
+print("[INFO] features matrix: {:.2f}MB".format(
+	features.nbytes / (1024 * 1000.0)))
+
+# partition the data into training and testing splits, using 75%
+# of the data for training and the remaining 25% for testing
+(trainRI, testRI, trainRL, testRL) = train_test_split(
+	rawImages, labels, test_size=0.25, random_state=42)
+
+
+print("[INFO] evaluating raw pixel accuracy...")
+model = KNeighborsClassifier(n_neighbors=args["neighbors"],
+	n_jobs=args["jobs"])
+model.fit(trainRI, trainRL)
+acc = model.score(testRI, testRL)
+print("[INFO] raw pixel accuracy: {:.2f}%".format(acc * 100))
+
